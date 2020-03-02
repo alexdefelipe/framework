05440e53b30c14cac92d8f42e70abaa52efc903d
Administrator <alexdefelipe@gmail.com>
2020-03-02 11:59:04 +0100

    Decouple several algorithms from training function: - Feedforward algorithm - Parameter optimization algorithm - Decision boundary tracker

--- core/modelo.py
+++ core/modelo.py
@@ -1,17 +1,18 @@
+import copy
 import os
 import tempfile
 
 import imageio
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
 
 from core.capas import Entrada
 from core.funciones import funciones_coste
+from core.optimizers import optimizers
 
 
 class Modelo:
-    def __init__(self, funcion_coste=funciones_coste.cross_entropy):
+    def __init__(self, funcion_coste=funciones_coste.cross_entropy, optimizer="SGD"):
         self.funcion_coste = funcion_coste
         self.capas = []
         self.W = []
@@ -19,33 +20,8 @@ class Modelo:
         self.deltas = []
         self.coste = []
         self.bound_tracking = []
-
-    def diagnose_boundaries(self, x_lims, y_lims, res, axis=None, plot=False, data=None):
-        import matplotlib.pyplot as plt
-
-        axis = axis or plt.gca()
-        x_values = np.linspace(x_lims[0], x_lims[1], res)
-        y_values = np.linspace(y_lims[0], y_lims[1], res)
-
-        grid = np.zeros((res, res))
-        inputs = []
-        for x in x_values:
-            for y in y_values:
-                inputs.append([x, y])
-        preds, scores = self.predict(inputs, return_scores=True)
-        scores = np.reshape(scores, (res, res)).T
-
-        axis.pcolormesh(x_values, y_values, scores, cmap="coolwarm")
-        axis.set_xlim(*x_lims)
-        axis.set_ylim(*y_lims)
-
-        if data is not None:
-            axis.scatter(data[0][:, 0], data[0][:, 1], c=data[1])
-
-        if plot:
-            plt.show()
-        else:
-            return axis
+        self.weights = []
+        self.optimizer = optimizers[optimizer]
 
     def add(self, capa):
         n_capas = len(self.capas)
@@ -73,85 +49,124 @@ class Modelo:
             raise Exception(
                 "La dimensión 0, correspondiente al número de muestras, de los inputs y de las etiquetas debe de coincidir ")
 
-    def train(self, inputs, targets, epochs=1, lr=0.001, batch_size=100, diagnose=False):
-        self.comprobar_entradas(inputs, targets)
-        self.coste = np.empty((epochs,))
+    def generate_random_batch(self, inputs, targets, batch_size):
+        batch_idx = np.random.choice(targets.size, batch_size, replace=False)
+        batch_inputs = inputs[batch_idx, :]
+        batch_targets = targets[batch_idx]
 
-        if diagnose:
-            fig, ax = plt.subplots(2, 1, figsize=(5, 6), gridspec_kw={'height_ratios': [1, 3]})
-            # ax = fig.add_subplot(111)
-            tmpdir = tempfile.gettempdir()
-            savepath = '/'.join([tmpdir, 'diagnostic_images'])
-            os.makedirs(savepath, exist_ok=True)
-            n = 0
+        return batch_inputs, batch_targets
+
+    def feed_forward(self, x, capas, W, b):
+        activaciones = np.reshape(x, (1, x.size))
+        for i, capa in enumerate(capas):
+            if i == 0:
+                activaciones = capa.__propagar__(activaciones)
+            else:
+                activaciones = capa.__propagar__(activaciones, W[i - 1], b[i - 1])
+
+        prediccion = int(np.round(activaciones))
+        score = activaciones
+
+        return prediccion, score
+
+    def train(self, inputs, targets, epochs=1, lr=0.001, batch_size=None, diagnose=False):
+        self.comprobar_entradas(inputs, targets)
+        self.cost = np.empty((epochs,))
+
+        if batch_size is None:
+            batch_size = targets.size
 
         for epo in range(epochs):
-            last_activations = np.empty(targets.shape)
-            for n_sample, (x, y) in enumerate(zip(inputs, targets)):
+            batch_inputs, batch_targets = self.generate_random_batch(inputs, targets, batch_size)
+            last_activations = np.empty(batch_targets.shape)
+            for n_sample, (x, y) in enumerate(zip(batch_inputs, batch_targets)):
                 # Feedforward
-                activaciones = np.reshape(x, (1, x.size))
-                for i, capa in enumerate(self.capas):
-                    if i == 0:
-                        activaciones = capa.__propagar__(activaciones)
-                    else:
-                        activaciones = capa.__propagar__(activaciones, self.W[i - 1], self.b[i - 1])
-                # Backpropagation
-                # Calcular deltas
+                self.feed_forward(x, self.capas, self.W, self.b)
 
-                delta = self.funcion_coste["derivada"](y, capa.a) * capa.funcion_activacion["derivada"](capa.z)
+                # Backpropagation
+                delta = self.funcion_coste["derivada"](y, self.capas[-1].a) * self.capas[-1].funcion_activacion[
+                    "derivada"](self.capas[-1].z)
                 self.deltas.insert(0, delta)
                 for i in reversed(range(1, len(self.capas) - 1)):
                     capa = self.capas[i]
                     delta = self.deltas[0] @ self.W[i].T * capa.funcion_activacion["derivada"](capa.z)
                     self.deltas.insert(0, delta)
-                # Gradient descent
-                for i in reversed(range(len(self.W))):
-                    self.W[i] = self.W[i] - lr * self.capas[i].a.T @ self.deltas[i]
-                    self.b[i] = self.b[i] - lr * self.deltas[i]
+
+                # Weight optimization
+                self.optimize_parameters(lr)
+
                 self.deltas = []
                 last_activations[n_sample] = self.capas[-1].a
-            self.coste[epo] = self.funcion_coste["funcion"](targets, last_activations)
-            if diagnose:
-                ax[0].plot(self.coste[:epo])
-                ax[0].set_title("Cost over epochs")
-                ax[0].set_xlim(0, epochs)
-                ax[0].set_xlabel("Epochs")
-                ax[0].set_ylabel("Cost")
-                ax[1] = self.diagnose_boundaries([min(inputs[:, 0]), max(inputs[:, 0])],
-                                                 [min(inputs[:, 1]), max(inputs[:, 1])], axis=ax[1], res=50,
-                                                 data=(inputs, targets))
-                ax[1].set_title("Decision map evolution")
-                # fig.suptitle("Training diagnostics")
-                fig.tight_layout()
-                # fig.subplots_adjust(top=0.92)
-                filename = '/'.join([savepath, 'image.jpg'])
-                plt.savefig(filename)
-                self.bound_tracking.append(imageio.imread(filename))
-                # plt.cla()
-                ax[0].cla()
-                ax[1].cla()
+            self.cost[epo] = self.funcion_coste["funcion"](batch_targets, last_activations)
+            self.weights.append(copy.deepcopy(self.W))
         if diagnose:
-            duration = [max(3 / epochs, 0.05) for i in range(epochs)]
-            duration[-1] = 1
-            imageio.mimsave('/'.join([savepath, 'anim.gif']), self.bound_tracking, duration=duration)
-            im = Image.open('/'.join([savepath, 'anim.gif']))
+            self.decision_boundary_tracking(inputs, targets)
 
-    def predict(self, inputs, return_scores=False):
+    def predict(self, inputs, weights=None, return_scores=False):
         predictions = []
         scores = []
+
+        if weights is None:
+            weights = self.W
+
         for x in inputs:
-            activaciones = x
-            # Feedforward
-            for i, capa in enumerate(self.capas[1:]):
-                if i == 0:
-                    activaciones = capa.__propagar__(x, self.W[i], self.b[i])
-                else:
-                    activaciones = capa.__propagar__(activaciones, self.W[i], self.b[i])
-            a = self.capas[-1].a[0]
-            if not np.isnan(a).any():
-                scores.append(a)
-                predictions.append(int(np.round(a)))
-        if return_scores:
-            return predictions, scores
+            prediction, score = self.feed_forward(x, self.capas, weights, self.b)
+            predictions.append(prediction)
+            if return_scores:
+                scores.append(score)
+        return (predictions, scores) if return_scores else predictions
+
+    def plot_probability_map(self, x_lims, y_lims, res, data=None, weights=None, axis=None, plot=False):
+        axis = axis or plt.gca()
+        x_values = np.linspace(x_lims[0], x_lims[1], res)
+        y_values = np.linspace(y_lims[0], y_lims[1], res)
+
+        inputs = np.array([(x, y) for x in x_values for y in y_values])
+        preds, scores = self.predict(inputs, weights=weights, return_scores=True)
+        scores = np.reshape(scores, (res, res)).T
+
+        axis.pcolormesh(x_values, y_values, scores, cmap="coolwarm")
+        axis.set_xlim(*x_lims)
+        axis.set_ylim(*y_lims)
+
+        if data is not None:
+            axis.scatter(data[0][:, 0], data[0][:, 1], c=data[1])
+
+        if plot:
+            plt.show()
         else:
-            return predictions
+            return axis
+
+    def decision_boundary_tracking(self, inputs, targets):
+        n_epochs = self.cost.size
+        fig, ax = plt.subplots(2, 1, figsize=(5, 6), gridspec_kw={'height_ratios': [1, 3]})
+        tmpdir = tempfile.gettempdir()
+        savepath = '/'.join([tmpdir, 'diagnostic_images'])
+        filename = '/'.join([savepath, 'image.jpg'])
+        os.makedirs(savepath, exist_ok=True)
+
+        for epoch in range(1, n_epochs + 1):
+            weights = self.weights[epoch - 1]
+            ax[0].set_title("Cost over epochs")
+            ax[0].set_xlabel("Epochs")
+            ax[0].set_ylabel("Cost")
+            ax[0].plot(self.cost[:epoch])
+            ax[0].set_xlim(0, epoch)
+            ax[1] = self.plot_probability_map([min(inputs[:, 0]), max(inputs[:, 0])],
+                                              [min(inputs[:, 1]), max(inputs[:, 1])], axis=ax[1], res=50,
+                                              data=(inputs, targets), weights=weights)
+
+            ax[1].set_title("Decision map evolution")
+            fig.tight_layout()
+            plt.savefig(filename)
+            self.bound_tracking.append(imageio.imread(filename))
+            ax[0].cla()
+            ax[1].cla()
+
+        duration = [max(3 / n_epochs, 0.05) for i in range(n_epochs)]
+        duration[-1] = 1
+        imageio.mimsave('/'.join([savepath, 'anim.gif']), self.bound_tracking, duration=duration)
+        print("Se ha guardado un GIF en " + savepath)
+
+    def optimize_parameters(self, lr):
+        self.optimizer(self, lr)
