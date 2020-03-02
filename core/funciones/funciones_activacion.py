351f8b94401260a01002fea2d5dde9b8440dfeab
Administrator <alexdefelipe@gmail.com>
2020-03-02 11:56:52 +0100

    Add ReLU function

--- core/funciones/funciones_activacion.py
+++ core/funciones/funciones_activacion.py
@@ -4,6 +4,7 @@ sigma = lambda x: 1 / (1 + np.exp(-x))
 identidad = {"funcion": lambda x: x, "derivada": 1}
 sigmoide = {"funcion": lambda x: sigma(x),
             "derivada": lambda x: sigma(x) * (1 - sigma(x))}
+relu = {"funcion": lambda x: np.maximum(0, x), "derivada": lambda x: np.where(x <= 0, 0, 1)}
 # t = lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 # tanh = {"funcion": lambda x: t(x),
 #         "derivada": lambda x: 1 - t(x) ** 2}
