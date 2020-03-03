527cf8d59c2ff3a34c3a7503887b09a8bb87f540
Administrator <alexdefelipe@gmail.com>
2020-03-03 21:33:13 +0100

    Added code to train with MNIST dataset

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.datasets import make_blobs
+from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
@@ -11,9 +11,9 @@ from core.capas import Entrada, Densa
 def nuestro_modelo(X_train, X_test, y_train, y_test):
     modelo = Modelo()
 
-    modelo.add(Entrada(2))
-    modelo.add(Densa(3))
-    modelo.add(Densa(3, funcion_activacion="softmax"))
+    modelo.add(Entrada(784))
+    modelo.add(Densa(32))
+    modelo.add(Densa(10, funcion_activacion="softmax"))
 
     modelo.train(X_train, y_train, epochs=100, lr=0.05, diagnose=True)
     # y_pred, scores = modelo.predict(X_test, True)
@@ -37,10 +37,12 @@ def nuestro_modelo(X_train, X_test, y_train, y_test):
 
 
 if __name__ == '__main__':
-    X, Y = make_blobs(n_samples=300, centers=3, n_features=2, random_state=105)
-    onehotencoder = OneHotEncoder(categories="auto")
-    Y = onehotencoder.fit_transform(np.expand_dims(Y, axis=1)).toarray()
-
+    # X, Y = make_blobs(n_samples=300, centers=3, n_features=2, random_state=105)
     # X, Y = make_circles(n_samples=1000, factor=0.1, noise=0.05, random_state=106)
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+    X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+    onehotencoder = OneHotEncoder(categories="auto")
+    y = onehotencoder.fit_transform(np.expand_dims(y, axis=1)).toarray()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=5000, test_size=10000, random_state=42)
     nuestro_modelo(X_train, X_test, y_train, y_test)
