ed5818f5ec498aff6f4953c43740e45eb3dfbf0f
Administrator <alexdefelipe@gmail.com>
2020-03-03 20:48:44 +0100

    Inserted softmax into a dictionary

--- core/funciones/funciones_activacion.py
+++ core/funciones/funciones_activacion.py
@@ -5,7 +5,7 @@ identidad = {"funcion": lambda x: x, "derivada": 1}
 sigmoide = {"funcion": lambda x: sigma(x),
             "derivada": lambda x: sigma(x) * (1 - sigma(x))}
 relu = {"funcion": lambda x: np.maximum(0, x), "derivada": lambda x: np.where(x <= 0, 0, 1)}
-def softmax(x): return x / np.sum(x)
+softmax = {"funcion": lambda x: x / np.sum(x)}
 # t = lambda x: (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
 # tanh = {"funcion": lambda x: t(x),
 #         "derivada": lambda x: 1 - t(x) ** 2}
