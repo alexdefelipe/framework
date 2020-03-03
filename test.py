d1f7518f71bde5ae3c636e71ff534b9b918f1e3f
Administrator <alexdefelipe@gmail.com>
2020-03-03 23:45:04 +0100

    Updated test file

--- test.py
+++ test.py
@@ -1,26 +1,29 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.datasets import fetch_openml
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from core import Modelo
 from core.capas import Entrada, Densa
 
 
-def nuestro_modelo(X_train, X_test, y_train, y_test):
+def nuestro_modelo(X_train, X_test, y_train, y_test, topologia):
     modelo = Modelo()
 
-    modelo.add(Entrada(784))
-    modelo.add(Densa(32))
-    modelo.add(Densa(10, funcion_activacion="softmax"))
+    modelo.add(Entrada(X_train.shape[1]))
+    for n in topologia:
+        modelo.add(Densa(n))
 
-    modelo.train(X_train, y_train, epochs=100, lr=0.05, diagnose=True)
-    # y_pred, scores = modelo.predict(X_test, True)
+    modelo.add(Densa(y_train.shape[1], funcion_activacion="softmax"))
+
+    modelo.train(X_train, y_train, epochs=100, batch_size=100, lr=0.05, diagnose=True)
+    y_pred, scores = modelo.predict(X_test, return_scores=True)
 
     # y_pred_bool = np.round(scores).astype(np.int)
-    # print("Resultados de nuestro modelo:\n")
-    # print(classification_report(y_test, np.squeeze(y_pred_bool)))
+    print("Resultados de nuestro modelo:\n")
+    print(classification_report(np.argmax(y_test, axis=1), y_pred))
 
     preds = modelo.predict(X_test)
 
@@ -37,12 +40,15 @@ def nuestro_modelo(X_train, X_test, y_train, y_test):
 
 
 if __name__ == '__main__':
-    # X, Y = make_blobs(n_samples=300, centers=3, n_features=2, random_state=105)
-    # X, Y = make_circles(n_samples=1000, factor=0.1, noise=0.05, random_state=106)
+    # X, y = make_blobs(n_samples=300, centers=3, n_features=3, random_state=105)
+    # X, y = make_circles(n_samples=1000, factor=0.1, noise=0.05, random_state=106)
     X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+    # X, y = load_digits(return_X_y=True)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
     onehotencoder = OneHotEncoder(categories="auto")
     y = onehotencoder.fit_transform(np.expand_dims(y, axis=1)).toarray()
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=5000, test_size=10000, random_state=42)
-    nuestro_modelo(X_train, X_test, y_train, y_test)
+    nuestro_modelo(X_train, X_test, y_train, y_test, [20])
